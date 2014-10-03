2e98714f779bc5588adea2b2981e3a067efb5875
Kritarie <seanamos@seanamos.net>
2014-10-03 00:25:14 -0400

    Created dialog builder for deck adding function Updated TODOs and created new deck dialog layout

--- FlashyCards/app/src/main/java/fc/flashycards/Home.java
+++ FlashyCards/app/src/main/java/fc/flashycards/Home.java
@@ -1,10 +1,12 @@
 package fc.flashycards;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class Home extends Activity {
 
@@ -12,6 +14,8 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //TODO Populate ListView with decks
     }
 
 
@@ -22,19 +26,31 @@ public class Home extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+    //Add deck dialog
     public void Add(MenuItem item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
+        //Define custom layout for dialog view
+        LayoutInflater inflater = this.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_new_deck, null));
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.dismiss();
+            }
+        });
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                //TODO Create new text file in external storage if edittext != empty && a file named edittext does not already exist
+            }
+        });
+
+        builder.setTitle(R.string.add_deck_title);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
